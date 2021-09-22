41ea1d3cc9bb06515b5382e512ca24257413fdf2
will-cesar <willcesar010421@gmail.com>
2021-09-21 22:30:19 -0300

    Aula 9 - primeiro teste e2e na home da app

--- devbook/cypress/integration/ui/devbook.spec.js
+++ devbook/cypress/integration/ui/devbook.spec.js
@@ -1,6 +1,51 @@
 describe("Devbook application", () => {
   it("Visits the DevBook", () => {
+    /* 
+      Teste para validar se dentro da home existe
+      uma tag de <h2> com "DevBook" escrito dentro dela
+    */
+
+    /* acessa a página */
     cy.visit("http://localhost:3000/");
+
+    /* 
+      Pega o elemento <h2> que contém o atributo data-test="heading"
+      e verifica se o mesmo contém a palavra "DevBook"
+    */
     cy.get('h2[data-test="heading"]').contains("DevBook");
+  });
+
+  it("Shows a book list", () => {
+    /* Teste para validar se uma lista de livros é exibida na home */
+
+    cy.visit("http://localhost:3000/");
+
+    /*
+      Pega um elemento <div> que contém o atributo data-test="book-list" e 
+      verifica se o mesmo existe
+    */
+    cy.get('div[data-test="book-list"]').should("exist");
+
+    /*
+      cy.get("div.book-item").should("have.length", 2);   
+      Pega os elementos <div> que contém a classe "book-item" e
+      verifica se existem 2 itens na tela 
+    */
+
+    /*
+      - Nesse teste se espera que cada item da lista tenha o título de "Refactoring" 
+      e "Domain-driven design" respectivamente
+      - pega todos os elementos <div> que contém a classe "book-item"
+      - se espera que contém apenas 2 itens, que são os "books"
+      - pega o título, ou seja, o texto de um elemento <h2> de cada item e verifica
+      se cada título é "Refactoring" e "Domain-driven design" respectivamente
+    */
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(2);
+      const titles = [...books].map(
+        (book) => book.querySelector("h2").innerHTML
+      );
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+    });
   });
 });
--- devbook/src/App.js
+++ devbook/src/App.js
@@ -6,6 +6,15 @@ function App() {
       <Typography variant="h2" component="h2" data-test="heading">
         DevBook!
       </Typography>
+
+      <div data-test="book-list">
+        <div className="book-item">
+          <h2 className="titles">Refactoring</h2>
+        </div>
+        <div className="book-item">
+          <h2 className="titles">Domain-driven design</h2>
+        </div>
+      </div>
     </>
   );
 }
