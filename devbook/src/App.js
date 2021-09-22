d4ee20f166cc00ed9fe743678c38b14a45463eb3
will-cesar <willcesar010421@gmail.com>
2021-09-21 22:46:24 -0300

    Aula 11 - refatoração da home criando componente de lista de livros

--- devbook/src/App.js
+++ devbook/src/App.js
@@ -1,28 +1,15 @@
 import { Typography } from "@material-ui/core";
+import { BookList } from "./components/BookList";
 
 function App() {
   const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
-
-  const renderBooks = (books) => {
-    return (
-      <div data-test="book-list">
-        {books.map((book) => (
-          <div className="book-item">
-            <Typography variant="h5" component="h5" data-test="heading">
-              {book.name}
-            </Typography>
-          </div>
-        ))}
-      </div>
-    );
-  };
 
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">
         DevBook!
       </Typography>
-      {renderBooks(books)}
+      <BookList books={books} />
     </>
   );
 }
